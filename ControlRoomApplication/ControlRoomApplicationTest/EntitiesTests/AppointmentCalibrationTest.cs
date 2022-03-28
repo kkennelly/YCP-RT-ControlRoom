8565356372727a86efa24b0c9d9668936cf3cf2d
jsnyder17 <jsnyder17@ycp.edu>
2022-03-28 13:23:17 -0400

    Added timer test to test timer duration. Test runs for 5 seconds as intended.

---
+++ ControlRoomApplication/ControlRoomApplicationTest/EntitiesTests/AppointmentCalibrationTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ControlRoomApplicationTest.EntitiesTests
+{
+    [TestClass]
+    public class AppointmentCalibrationTest
+    {
+        Timer timer;
+        bool exit = false;
+
+        [TestMethod]
+        public void TimerTest()
+        {
+            timer = new Timer(5000);
+            timer.Elapsed += new ElapsedEventHandler(TimerEvent);
+
+            timer.Start();
+
+            while (!exit)
+            {
+                // Do stuff 
+            }
+
+            Assert.IsTrue(exit);
+        }
+
+        public void TimerEvent(Object o, ElapsedEventArgs e)
+        {
+            exit = true;
+        }
+    }
+}
